f9d074c3056ff997a7ec56bb6cbc0acb1a0efffb
Vieira Da Rocha <vieira.asier@uni.lan>
2022-09-29 09:57:02 +0200

    jueves 29 septiembre

---
+++ UD1/1-INTRODUCTION/src/ArithmeticTest2.java
@@ -0,0 +1,14 @@
+public class ArithmeticTest2 {
+    public static void main(String[] args) {
+
+      int number1 = 1; 
+      int number2 = 2; 
+      int product;
+
+     product = 31 * number1 + 17 * number2;
+      
+      System.out.print("The sum of 31 times of number1 and 17 times of number2 is");
+      System.out.print (" ");
+      System.out.print(product);      
+    }
+}
---
+++ UD1/1-INTRODUCTION/src/SumProduct3Numbers.java
@@ -0,0 +1,19 @@
+public class SumProduct3Numbers {
+    public static void main(String[] args) {
+
+      int number1 = 1; 
+      int number2 = 2; 
+      int number3 = 77;
+      int product;
+      int sum;
+
+     product =  number1 * number2 * number3;
+     sum = number1 + number2 + number3;
+      
+      System.out.print("The sum of number1, number2 and number3 is ");
+      System.out.print(sum);
+      System.out.print(" and ");
+      System.out.print ("The product of number 1, number2 and number3 is ");
+      System.out.print(product);   
+    }
+}
